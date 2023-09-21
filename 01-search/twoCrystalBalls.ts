57074e61b613871d9bc2b2484f84ebde7f7dad77
ch2isk4kos <ch2isk4kos@gmail.com>
2023-09-21 14:46:56 -0400

    add test(s)

--- 01-search/twoCrystalBalls.ts
+++ 01-search/twoCrystalBalls.ts
@@ -1,3 +1,15 @@
+/**
+ * 
+ * @param breaks 
+ * @returns number
+ * 
+ * 
+ */
+
 export default function twoCrystalBalls(breaks: boolean[]): number {
   return -1;
 }
+
+const breaks = [false, false, true, false, false];
+const result = twoCrystalBalls(breaks);
+console.log(result); //
