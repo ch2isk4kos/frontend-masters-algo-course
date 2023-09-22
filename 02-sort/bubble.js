741908adc7adac68955b04105c125287bbdf60a6
ch2isk4kos <ch2isk4kos@gmail.com>
2023-09-22 16:35:47 -0400

    compile bubble.ts

---
+++ 02-sort/bubble.js
@@ -0,0 +1,26 @@
+"use strict";
+// O(N^2)
+Object.defineProperty(exports, "__esModule", { value: true });
+function swap(arr, i, j) {
+    var tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
+}
+function bubbleSort(arr) {
+    console.log(arr);
+    for (var i = 0; i < arr.length; ++i) {
+        for (var j = 0; j < arr.length - 1 - i; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                //  swap(arr, arr[j], arr[j+1])
+                var tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
+    console.log(arr); // [ 1, 2, 3, 4, 7 ]
+}
+exports.default = bubbleSort;
+var arr = [1, 3, 7, 4, 2];
+var result = bubbleSort(arr);
+console.log(result); // undefined
--- 02-sort/bubble.ts
+++ 02-sort/bubble.ts
@@ -1,20 +1,27 @@
 // O(N^2)
 
-function swap(arr, j) {
-  const tmp = arr[j];
-  arr[j] = arr[j + 1];
-  arr[j + 1] = tmp;
-}
+// @NOTE: not currently working
+// function swap(arr: number[], i: number, j: number) {
+//   const tmp = arr[i];
+//   arr[i] = arr[j];
+//   arr[j] = tmp;
+// }
 
-export default function bubbleSort(arr: number[]): void {
+export default function bubbleSort(arr: number[]): void {  
   for (let i = 0; i < arr.length; ++i) {
     for (let j = 0; j < arr.length - 1 - i; ++j) {
       if (arr[j] > arr[j+1]) {
-       swap(arr, arr[j])
+      //  swap(arr, arr[j], arr[j+1])
+        const tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
       }
     }
   }
+  console.log(arr); // [ 1, 2, 3, 4, 7 ]
 }
 
 const arr = [ 1, 3, 7, 4, 2 ];
-const result = bubbleSort(arr); // 
+const result = bubbleSort(arr);
+console.log(result); // undefined
+
