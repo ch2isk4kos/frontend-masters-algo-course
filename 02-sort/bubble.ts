f64dbce57b046637bd7996c8747aacc61cd829fb
ch2isk4kos <ch2isk4kos@gmail.com>
2023-09-22 16:12:08 -0400

    implemented bubble sort algorithm

--- 02-sort/bubble.ts
+++ 02-sort/bubble.ts
@@ -1,3 +1,20 @@
-export default function bubbleSort(arr: number[]): void {
+// O(N^2)
 
+function swap(arr, j) {
+  const tmp = arr[j];
+  arr[j] = arr[j + 1];
+  arr[j + 1] = tmp;
 }
+
+export default function bubbleSort(arr: number[]): void {
+  for (let i = 0; i < arr.length; ++i) {
+    for (let j = 0; j < arr.length - 1 - i; ++j) {
+      if (arr[j] > arr[j+1]) {
+       swap(arr, arr[j])
+      }
+    }
+  }
+}
+
+const arr = [ 1, 3, 7, 4, 2 ];
+const result = bubbleSort(arr); // 
