b5ac7d3215cedaaa57a8f6d0a9b4649ed9cb4b57
ch2isk4kos <ch2isk4kos@gmail.com>
2023-09-22 17:35:15 -0400

    add interface for linked list

--- 02-sort/linkedList.ts
+++ 02-sort/linkedList.ts
@@ -1,27 +1,20 @@
+// interface LinkedList<T> {
+//   get length(): number;
+//   insertAt(item: T, index: number): void;
+//   remove(item: T): T | undefined;
+//   removeAt(index: number): T | undefined;
+//   append(item: T): void;
+//   prepend(item: T): void;
+//   get(index: number): T | undefined;
+// }
+
 export default class DoublyLinkedList<T> {
     public length: number;
-
-    
-
-    constructor() {
-    }
-
-    prepend(item: T): void {
-
-}
-    insertAt(item: T, idx: number): void {
-
-}
-    append(item: T): void {
-
-}
-    remove(item: T): T | undefined {
-
-}
-    get(idx: number): T | undefined {
-
-}
-    removeAt(idx: number): T | undefined {
-
-}
+    constructor() {}
+    prepend(item: T): void {}
+    insertAt(item: T, idx: number): void {}
+    append(item: T): void {}
+    remove(item: T): T | undefined {}
+    get(idx: number): T | undefined {}
+    removeAt(idx: number): T | undefined {}
 }
