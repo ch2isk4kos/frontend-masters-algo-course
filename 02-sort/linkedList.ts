f19199d11123c035c2df361d4fd5715ba01f665d
ch2isk4kos <ch2isk4kos@gmail.com>
2023-09-23 11:59:41 -0400

    create class for a list node

--- 02-sort/linkedList.ts
+++ 02-sort/linkedList.ts
@@ -1,20 +1,23 @@
-// interface LinkedList<T> {
-//   get length(): number;
-//   insertAt(item: T, index: number): void;
-//   remove(item: T): T | undefined;
-//   removeAt(index: number): T | undefined;
-//   append(item: T): void;
-//   prepend(item: T): void;
-//   get(index: number): T | undefined;
+// 
+// export default class DoublyLinkedList<T> {
+//     public length: number;
+//     constructor() {}
+//     prepend(item: T): void {}
+//     insertAt(item: T, idx: number): void {}
+//     append(item: T): void {}
+//     remove(item: T): T | undefined {}
+//     get(idx: number): T | undefined {}
+//     removeAt(idx: number): T | undefined {}
 // }
 
-export default class DoublyLinkedList<T> {
-    public length: number;
-    constructor() {}
-    prepend(item: T): void {}
-    insertAt(item: T, idx: number): void {}
-    append(item: T): void {}
-    remove(item: T): T | undefined {}
-    get(idx: number): T | undefined {}
-    removeAt(idx: number): T | undefined {}
+class ListNode<T> {
+  public value: any;
+  public next: ListNode<T>;
+  public prev: ListNode<T>;
+}
+
+export class DoublyLinkedList {
+  constructor( value: any = null) {
+    // 
+  }
 }
