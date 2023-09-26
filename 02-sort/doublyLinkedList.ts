c39fd79c301687df4cd545e8a1c44040274fb1e4
ch2isk4kos <ch2isk4kos@gmail.com>
2023-09-26 16:37:08 -0400

    create class to describe a node

--- 02-sort/doublyLinkedList.ts
+++ 02-sort/doublyLinkedList.ts
@@ -1,11 +1,17 @@
 /**
- * Doubly Linked List Implementation
- * 
- * A doubly linked list is a data structure which contain a head, tail and length property,
- * and consist of nodes, each of which store a value and a pointer to the next/previous node.
+ * List Node Class
+ * Represents a node used in a doubly linked list.
  * 
  * @template T is the type of a node's value.
- * @property head is the beginning of the list.
- * @property tail is the end of the list.
- * @property length is the size of the list.
+ * @property value is the data stored.
+ * @property prev is a pointer to the previous node in the list.
+ * @property next is a pointer to the next sequential node in the list.
  */
+
+export class ListNode<T> {
+  constructor(
+    public value: T,
+    public prev: ListNode<T> | null = null,
+    public next: ListNode<T> | null = null,
+  ) {}
+}
