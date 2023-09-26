9be26c63c2ca26c62777372c4c8c9c4bd11e2748
ch2isk4kos <ch2isk4kos@gmail.com>
2023-09-26 16:26:23 -0400

    create file for doubly linked list implementation

---
+++ 02-sort/doublyLinkedList.ts
@@ -0,0 +1,11 @@
+/**
+ * Doubly Linked List Implementation
+ * 
+ * A doubly linked list is a data structure which contain a head, tail and length property,
+ * and consist of nodes, each of which store a value and a pointer to the next/previous node.
+ * 
+ * @template T is the type of a node's value.
+ * @property head is the beginning of the list.
+ * @property tail is the end of the list.
+ * @property length is the size of the list.
+ */
