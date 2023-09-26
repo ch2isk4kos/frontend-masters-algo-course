e905c89000cf9db10282d39de96893d3fba31dc2
ch2isk4kos <ch2isk4kos@gmail.com>
2023-09-26 16:38:20 -0400

    create boilerplate for doubly linked list class && set private variables

--- 02-sort/doublyLinkedList.ts
+++ 02-sort/doublyLinkedList.ts
@@ -15,3 +15,21 @@ export class ListNode<T> {
     public next: ListNode<T> | null = null,
   ) {}
 }
+
+/**
+ * Doubly Linked List Implementation
+ * 
+ * A doubly linked list is a data structure which contain a head, tail and length property,
+ * and consist of nodes, each of which store a value and a pointer to the next/previous node.
+ * 
+ * @template T is the type of a node's value.
+ * @property head is the beginning of the list.
+ * @property tail is the end of the list.
+ * @property length is the size of the list.
+ */
+
+export class DoublyLinkedList<T> {
+  private head?: ListNode<T> | null = null;
+  private tail?: ListNode<T> | null = null;
+  private length: number = 0;
+}
